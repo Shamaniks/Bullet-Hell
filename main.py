4af561e6c627bb796462506b2a4252cb4ef1b447
Shamaniks <78531044+Shamaniks@users.noreply.github.com>
2022-12-12 18:21:22 +0900

    Коммит №2 Работа с гит

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import pygame
 
-"""Инициализация физических величин"""
-gravity_acceleration = 10 # Ускорение свободного падения
 
-"""Инициализация процесса"""
-TPS = 60 # Обновления (tick) в секунду
-pygame.init()
-pygame.display.set_caption("It's time for bullet hell")
-size = width, height = 1920, 1080
-screen = pygame.display.set_mode(size)
-running = True
-while running:
-    for event in pygame.event.get():
-        if event == pygame.QUIT:
-            running = False
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
-pygame.quit()
+def main():
+    """Инициализация физических величин"""
+    gravity_acceleration = 10 # Ускорение свободного падения
+
+    """Инициализация процесса"""
+    TPS = 60 # Обновления (tick) в секунду
+    pygame.init()
+    pygame.display.set_caption("It's time for bullet hell")
+    size = width, height = 1920, 1080
+    screen = pygame.display.set_mode(size)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+    pygame.quit()
+
+
+main()
